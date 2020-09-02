2ff932159f1dd15c30f1e5c0a26d45e97f7df15e
Danilo Patanè <d.patane98@gmail.com>
2020-09-02 16:35:26 +0200

    Commenti aggiunti

--- main.js
+++ main.js
@@ -1,33 +1,48 @@
 $(document).ready(function()
 {
-var slide = $(".images .slide");
-var dot = $(".nav .fa-circle");
+  // dichiaro due variabili che mi torneranno utili più tardi
+  var slide = $(".images .slide");
+  var dot = $(".nav .fa-circle");
+
+  // Imposto cosa accadrà quando cliccheranno la freccietta per andare avanti
   $('.next').click(
     function() {
+      // mi richiamo le funzioni che ho già dichiarato
+
       Next();
       dotNext();
     }
   );
+  // Imposto cosa accadrà quando cliccheranno la freccietta per andare indietro
 
   $('.prev').click(
     function() {
+      // mi richiamo le funzioni che ho già dichiarato
+
       Prev();
       dotPrev();
     }
   )
-  
-  dot.click(function () {
-
-  var indeX=$(this).index();
-  var currentSlide=slide.eq(indeX);
-  currentSlide.addClass('active');
-  $(this).addClass('active');
-
-  	$(".images img").not(currentSlide).removeClass('active');
-  	$(".fa-circle").not(this).removeClass('active');
+  // Imposto cosa accade quando si clicca un pallino invece della freccia
+  dot.click(
+    function () {
+      // dichiaro una variabile che mi fa ottenere un numero tra 0 e 3 (in questo caso) grazie alla funzione index di jquery e l'utilizzo del this il numero ottenuto varia rispetto a quello che clicca l'utente
+      var indeX=$(this).index();
+      console.log(indeX);
+      // Riutilizzo la variabile che ho dichiarato all'inizio che non è altro che la raccolta delle immagini e grazie ad eq() (che mi fa trovare dato un numero l'elemento che corrisponde a quel numero in una lista di elementi) riesco a far capire al programma quale slide deve cercare
+      var currentSlide=slide.eq(indeX);
+      console.log(currentSlide);
+      // Alla slide trovata gli do la classe active
+      currentSlide.addClass('active');
+      // e do la classe active anche al bottone premuto
+      $(this).addClass('active');
+        // e all'immagine che non è la slide che mi serve gli rimuovo la classe active facendo la stessa cosa al bottone
+      	$(".images img").not(currentSlide).removeClass('active');
+      	$(".fa-circle").not(this).removeClass('active');
   });
 })
-
+// Inizio funzioni
+// funzione per andare avanti nel carousel
 function Next() {
   var imgActive = $("img.active");
   imgActive.removeClass("active");
@@ -40,6 +55,7 @@ function Next() {
   nextImg.addClass("active");
 }
 
+// funzione per andare indietro nel carousel
 
 function Prev() {
   var imgActive = $("img.active");
@@ -52,7 +68,7 @@ function Prev() {
   }
   nextImg.addClass("active");
 }
-
+// funzione per andare avanti con i pallini quando si clicca la freccia
 function dotNext() {
   var dotActive = $("i.active");
   dotActive.removeClass("active");
@@ -65,6 +81,7 @@ function dotNext() {
   nextDot.addClass("active");
 }
 
+// funzione per andare indietro con i pallini quando si clicca la freccia
 
 function dotPrev() {
   var dotActive = $("i.active");
@@ -77,10 +94,4 @@ function dotPrev() {
   }
   nextDot.addClass("active");
 }
-
-// function dotClick() {
-//   var puntoCliccato = $(this).children()
-// if () {
-//
-// }
-// }
+// fine funzioni
